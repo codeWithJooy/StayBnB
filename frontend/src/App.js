6953ea3ea654034d21831d8090afd664d166a74a
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-04-12 21:58:26 +0530

    Signup Page Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SignUp from "./Pages/Signup/Signup";
 function App() {
   return (
-    <div className="App">
-      <div></div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={SignUp} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
