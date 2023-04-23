a54a24e6130d15b52306fff36f5b569ee71c6a60
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-04-23 21:23:55 +0530

    Root Css Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { SignUp } from "./Pages/Signup/Signup";
+import { Home } from "./Pages/Home/Home";
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={SignUp} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/" exact component={Home} />
       </Switch>
     </BrowserRouter>
   );
